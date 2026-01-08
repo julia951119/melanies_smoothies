b317515561c0c292bf280510ccfab54ba1b1aeec
julia951119 <75391032+julia951119@users.noreply.github.com>
2026-01-08 12:06:42 +0000

    Update streamlit_app.py
    added import requests and watermelon

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,9 @@ my_insert_stmt = f"""
 INSERT INTO smoothies.public.orders (ingredients, name_on_order)
 VALUES ('{ingredients_string}', '{name_on_order}')
 """
-
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 #st.write(my_insert_stmt)
 
 time_to_insert = st.button('Submit Order')
