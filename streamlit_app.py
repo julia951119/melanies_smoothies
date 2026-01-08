0d41a7386b6c4cbeb20e6a549fc65171aeda9c02
julia951119 <75391032+julia951119@users.noreply.github.com>
2026-01-08 12:11:13 +0000

    changed the displayed of nutritional info

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,8 @@ VALUES ('{ingredients_string}', '{name_on_order}')
 """
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
+sf_df= st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 #st.write(my_insert_stmt)
 
 time_to_insert = st.button('Submit Order')
